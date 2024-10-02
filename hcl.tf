03f30944a18b60c05bc656c9c64526cf7f7fe8ba
vikramadityadevarampati <vikramadityadevarampati@gmail.com>
2024-10-02 10:05:59 +0530

    aws

---
+++ hcl.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformExample"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+}
